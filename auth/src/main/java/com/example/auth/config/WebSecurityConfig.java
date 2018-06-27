8813644a6654f747adcbbe7f3a57b7f8189bebdd
IuliaGruia <gruia_iulia@yahoo.com>
2018-06-27 15:22:20 +0300

    add oauth2

---
+++ auth/src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authExcep) ->
+                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+                )
+                .and().authorizeRequests().antMatchers("/**").authenticated()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("123456789")
+                .authorities("ADMIN")
+                .and()
+                .withUser("user")
+                .password("123456789")
+                .authorities("USER");
+    }
+}
